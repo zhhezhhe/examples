f154615b2ed9cb9bd0cfea576a5321962551a455
AIChallenger <zhenghe@interns.chuangxin.com>
2018-01-31 17:04:28 +0800

    num class 57

--- imagenet/finetuen.sh
+++ imagenet/finetuen.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env bash
 outpath=train_log.log
 
+#python main.py -a resnet101 \
+#    ../../data_cnn_transfer/ \
+#    --pretrained #> ${outpath} 2>&1 &
+
 python main.py -a resnet101 \
-    ../../data_cnn_transfer/ \
-    --pretrained #> ${outpath} 2>&1 &
+    ../../temp/ \
+    --pretrained \
+    --num_classes 57 #> ${outpath} 2>&1 &
